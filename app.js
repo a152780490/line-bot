f0fe63f0850e17de505a7eae21f9f76eceaa2e0e
Web Peasant <123830086+a152780490@users.noreply.github.com>
2025-06-21 16:25:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-  channelAccessToken:process.env. 'miteCrm5LO12yKC3bkA0JUyXJmG1Ij7njpw7gnBmy5a6tHvdh256tKfcDHLC8FAk8ukcU8FDVbW3jdxgjYMsvrVKsUk90Up6WYAcBgcuW+u2bf4+HQo150rAqoazAlRDux3XoIGDqR93usVORUcpbwdB04t89/1O/w1cDnyilFU=',
-  channelSecret: process.env.'2331849964c6f9dad76337152a665c61',
+  channelAccessToken: 'miteCrm5LO12yKC3bkA0JUyXJmG1Ij7njpw7gnBmy5a6tHvdh256tKfcDHLC8FAk8ukcU8FDVbW3jdxgjYMsvrVKsUk90Up6WYAcBgcuW+u2bf4+HQo150rAqoazAlRDux3XoIGDqR93usVORUcpbwdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '2331849964c6f9dad76337152a665c61',
 };
 
 const client = new Client(config);
